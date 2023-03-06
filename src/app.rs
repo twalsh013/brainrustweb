3b62c43b68d28be8c3955a3ddd065cab3d95aa4f
Taylor Walsh <Stephen.Walsh@gtri.gatech.edu>
2023-03-06 11:48:14 -0500

    Finally got file reading working the way I want. Kinda scuffed though, can clean up A LOT

--- src/app.rs
+++ src/app.rs
@@ -1,14 +1,22 @@
 //use gloo::{self, console}; //, file};
 use std::fs::File;
-use std::io::{BufReader, Read};
+use std::io::{BufReader, Read, Empty};
 use std::str;
 use std::path::Path;
+use std::sync::mpsc::{Receiver, TryRecvError};
 use rfd::AsyncFileDialog;
 use wasm_bindgen::JsValue;
 use web_sys::console;
 use web_sys::console::log_1;
-use std::sync::{Arc,Mutex};
+use std::sync::{Arc,Mutex,mpsc::channel};
+use std::sync::mpsc::Sender;
 use std::{thread, time};
+use lazy_static::lazy_static;
+
+lazy_static!{
+    static ref MYCHAN: Mutex<(Sender<Vec<u8>>,Receiver<Vec<u8>>)> = Mutex::new(channel());
+}
+
 //use eframe::{egui,epi};
 //use std::io::prelude::*;
 /// We derive Deserialize/Serialize so we can persist app state on shutdown.
@@ -20,7 +28,6 @@ pub struct TemplateApp {
     result: String,
     // this how you opt-out of serialization of a member
     //#[serde(skip)]
-    //value: f32,
 }
 
 const TAPE_SIZE: usize = 30000;
@@ -120,7 +127,7 @@ impl eframe::App for TemplateApp {
     /// Put your widgets into a `SidePanel`, `TopPanel`, `CentralPanel`, `Window` or `Area`.
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         let Self { label, result } = self; //, value } = self;
-
+        
         // Examples of how to create different panels and windows.
         // Pick whichever suits you.
         // Tip: a good default choice is to just keep the `CentralPanel`.
@@ -145,34 +152,54 @@ impl eframe::App for TemplateApp {
                 ui.label("Enter some Brain* code: ");
                 ui.text_edit_singleline(label);
             });
-
+            
             //let mut result = String::new();
+            static mut CHECKED: bool = false;
+            
 
             if ui.button("Run").clicked() {
                 let contents = label.clone();
                 
-                *result = interpret(contents);
-                let mut answer:Arc<Mutex<Vec<u8>>> = Arc::new(Mutex::new(vec![]));
-                let answerb = answer.clone();
+                unsafe {
+                    CHECKED = true;
+                }
+                //let txclone = tx.clone();
+                //let mut answer:Arc<Mutex<Vec<u8>>> = Arc::new(Mutex::new(vec![]));
+                //let answerb = answer.clone();
                 let task = rfd::AsyncFileDialog::new().pick_file();
                 wasm_bindgen_futures::spawn_local(async move{
-                    let mut answerclone = answer.clone();
-                    let mut stringy = answerclone.lock().unwrap();
+                    //let mut answerclone = answer.clone();
+                    //let mut stringy = answerclone.lock().unwrap();
+                    let txclone = MYCHAN.lock().unwrap().0.clone();
                     let file = task.await;
                     if let Some(file) = file {
+                        log_1(&"gotfile".into());
                         // If you care about wasm support you just read() the file
                         let mystring = file.read().await;
-                        
-                        *stringy = mystring.clone();//= file.read().await.into();
+                        txclone.send(mystring).unwrap();
+                        //*stringy = mystring.clone();//= file.read().await.into();
                         //let mystring = String::from_utf8(file.read().await).unwrap();
                         
-                        let js: JsValue = String::from_utf8(mystring).unwrap().into();
-                        log_1(&"inner stuff".into());
-                        log_1(&js);      
+                        //let js: JsValue = String::from_utf8(mystring).unwrap().into();
+                        //log_1(&"inner stuff".into());
+                        //log_1(&js);      
+                        //drop(txclone);
+                    } else {
+                        log_1(&"not file".into());
                     }
                });
 
-                let tmp = answerb.lock().unwrap();
+            //    if let Ok(msg) = rx.recv() {
+            //     //let jsmsg: JsValue = String::from_utf8(msg).unwrap().into();
+            //     log_1(&"something".into());
+            //     //log_1(&jsmsg);
+            //    } else {
+            //     log_1(&"oops".into());
+            //    }
+
+               *result = interpret(contents);
+
+                //let tmp = answerb.lock().unwrap();
                 // let myvec = match tmp {
                 //     Ok(vec) => {
                 //         log_1(&"vector".into());
@@ -185,10 +212,10 @@ impl eframe::App for TemplateApp {
                 //         vect
                 //     },
                 // };
-                let myvec = tmp.to_vec();//(&*tmp.lock().unwrap()).to_vec();
-                log_1(&"tried some stuff".into());
-                let js: JsValue = String::from_utf8(myvec).unwrap().into();
-                log_1(&js);
+                //let myvec = tmp.to_vec();//(&*tmp.lock().unwrap()).to_vec();
+                //log_1(&"tried some stuff".into());
+                //let js: JsValue = String::from_utf8(myvec).unwrap().into();
+                //log_1(&js);
                 //*result = String::from_utf8(myvec).unwrap();
                 
                 //if let Some(picked_path) = picked_path {
@@ -198,6 +225,29 @@ impl eframe::App for TemplateApp {
                 });
                 //}
             }
+
+            //let mut stringprint = String::new();
+
+            unsafe {
+                if CHECKED {
+                    log_1(&"checkum".into());
+                    match MYCHAN.lock().unwrap().1.try_recv() {
+                        Ok(msg) => {
+            
+                            let stmsg = String::from_utf8(msg).unwrap();
+                            *result = stmsg.clone();
+                            let jsmsg: JsValue = stmsg.into();
+                            log_1(&jsmsg);
+                            log_1(&"something".into());
+                            //log_1(&jsmsg);
+                            CHECKED = false;
+                        },
+                        Err(TryRecvError::Empty) => log_1(&"no".into()),
+                        Err(TryRecvError::Disconnected) => log_1(&"no2".into()),
+                    }        
+                }
+            }
+            
 
             //ui.with_layout(egui::Layout::bottom_up(egui::Align::LEFT), |ui| {
             ui.horizontal(|ui| {
@@ -210,6 +260,18 @@ impl eframe::App for TemplateApp {
             });
             //});
         });
+
+        // match RXG.try_recv() {
+        //     Ok(msg) => {
+            
+        //     //let jsmsg: JsValue = String::from_utf8(msg).unwrap().into();
+        //     //log_1(&msg.to_string().into());
+        //     log_1(&"something".into());
+        //     //log_1(&jsmsg);
+        //     },
+        //     Err(TryRecvError::Empty) => log_1(&"no".into()),
+        //     Err(TryRecvError::Disconnected) => log_1(&"no2".into()),
+        // }
 
         if false {
             egui::Window::new("Window").show(ctx, |ui| {
