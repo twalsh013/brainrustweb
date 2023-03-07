31576c27c3fb08d9c612fefc7569dbaa3397a464
Taylor Walsh <tw@taylorwalsh.dev>
2023-03-06 21:20:38 -0500

    Fixed clippy and fmt stuff. This thing works, no check var needed

--- src/app.rs
+++ src/app.rs
@@ -1,20 +1,22 @@
 //use gloo::{self, console}; //, file};
-use std::fs::File;
-use std::io::{BufReader, Read, Empty};
-use std::str;
-use std::path::Path;
-use std::sync::mpsc::{Receiver, TryRecvError};
-use rfd::AsyncFileDialog;
-use wasm_bindgen::JsValue;
-use web_sys::console;
-use web_sys::console::log_1;
-use std::sync::{Arc,Mutex,mpsc::channel};
-use std::sync::mpsc::Sender;
-use std::{thread, time};
 use lazy_static::lazy_static;
+use rfd::AsyncFileDialog;
+//use std::fs::File;
+//use std::io::{BufReader, Empty, Read};
+//use std::path::Path;
+use std::str;
+use std::sync::mpsc::Receiver;
+use std::sync::mpsc::Sender;
+use std::sync::{mpsc::channel, Mutex};
+//use std::{thread, time};
+use wasm_bindgen::JsValue;
+//use web_sys::console;
+use web_sys::console::log_1;
 
-lazy_static!{
-    static ref MYCHAN: Mutex<(Sender<Vec<u8>>,Receiver<Vec<u8>>)> = Mutex::new(channel());
+type StaticChanPair = Mutex<(Sender<Vec<u8>>, Receiver<Vec<u8>>)>;
+
+lazy_static! {
+    static ref MYCHAN: StaticChanPair = Mutex::new(channel());
 }
 
 //use eframe::{egui,epi};
@@ -127,7 +129,7 @@ impl eframe::App for TemplateApp {
     /// Put your widgets into a `SidePanel`, `TopPanel`, `CentralPanel`, `Window` or `Area`.
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         let Self { label, result } = self; //, value } = self;
-        
+
         // Examples of how to create different panels and windows.
         // Pick whichever suits you.
         // Tip: a good default choice is to just keep the `CentralPanel`.
@@ -152,22 +154,21 @@ impl eframe::App for TemplateApp {
                 ui.label("Enter some Brain* code: ");
                 ui.text_edit_singleline(label);
             });
-            
+
             //let mut result = String::new();
-            static mut CHECKED: bool = false;
-            
+            //static mut CHECKED: bool = false;
 
             if ui.button("Run").clicked() {
                 let contents = label.clone();
-                
-                unsafe {
-                    CHECKED = true;
-                }
+
+                //unsafe {
+                //    CHECKED = true;
+                //}
                 //let txclone = tx.clone();
                 //let mut answer:Arc<Mutex<Vec<u8>>> = Arc::new(Mutex::new(vec![]));
                 //let answerb = answer.clone();
-                let task = rfd::AsyncFileDialog::new().pick_file();
-                wasm_bindgen_futures::spawn_local(async move{
+                let task = AsyncFileDialog::new().pick_file();
+                wasm_bindgen_futures::spawn_local(async move {
                     //let mut answerclone = answer.clone();
                     //let mut stringy = answerclone.lock().unwrap();
                     let txclone = MYCHAN.lock().unwrap().0.clone();
@@ -179,25 +180,25 @@ impl eframe::App for TemplateApp {
                         txclone.send(mystring).unwrap();
                         //*stringy = mystring.clone();//= file.read().await.into();
                         //let mystring = String::from_utf8(file.read().await).unwrap();
-                        
+
                         //let js: JsValue = String::from_utf8(mystring).unwrap().into();
                         //log_1(&"inner stuff".into());
-                        //log_1(&js);      
+                        //log_1(&js);
                         //drop(txclone);
                     } else {
                         log_1(&"not file".into());
                     }
-               });
+                });
 
-            //    if let Ok(msg) = rx.recv() {
-            //     //let jsmsg: JsValue = String::from_utf8(msg).unwrap().into();
-            //     log_1(&"something".into());
-            //     //log_1(&jsmsg);
-            //    } else {
-            //     log_1(&"oops".into());
-            //    }
+                //    if let Ok(msg) = rx.recv() {
+                //     //let jsmsg: JsValue = String::from_utf8(msg).unwrap().into();
+                //     log_1(&"something".into());
+                //     //log_1(&jsmsg);
+                //    } else {
+                //     log_1(&"oops".into());
+                //    }
 
-               *result = interpret(contents);
+                *result = interpret(contents);
 
                 //let tmp = answerb.lock().unwrap();
                 // let myvec = match tmp {
@@ -217,7 +218,7 @@ impl eframe::App for TemplateApp {
                 //let js: JsValue = String::from_utf8(myvec).unwrap().into();
                 //log_1(&js);
                 //*result = String::from_utf8(myvec).unwrap();
-                
+
                 //if let Some(picked_path) = picked_path {
                 ui.horizontal(|ui| {
                     ui.label("Picked file:");
@@ -228,26 +229,24 @@ impl eframe::App for TemplateApp {
 
             //let mut stringprint = String::new();
 
-            unsafe {
-                if CHECKED {
-                    log_1(&"checkum".into());
-                    match MYCHAN.lock().unwrap().1.try_recv() {
-                        Ok(msg) => {
-            
-                            let stmsg = String::from_utf8(msg).unwrap();
-                            *result = stmsg.clone();
-                            let jsmsg: JsValue = stmsg.into();
-                            log_1(&jsmsg);
-                            log_1(&"something".into());
-                            //log_1(&jsmsg);
-                            CHECKED = false;
-                        },
-                        Err(TryRecvError::Empty) => log_1(&"no".into()),
-                        Err(TryRecvError::Disconnected) => log_1(&"no2".into()),
-                    }        
-                }
-            }
-            
+            //unsafe {
+            //if CHECKED {
+            //log_1(&"checkum".into());
+            //match MYCHAN.lock().unwrap().1.try_recv() {
+            if let Ok(msg) = MYCHAN.lock().unwrap().1.try_recv() {
+                let stmsg = String::from_utf8(msg).unwrap();
+                *result = stmsg.clone();
+                let jsmsg: JsValue = stmsg.into();
+                log_1(&jsmsg);
+                log_1(&"something".into());
+                //log_1(&jsmsg);
+                //          CHECKED = false;
+            };
+            //Err(TryRecvError::Empty) => log_1(&"no".into()),
+            //Err(TryRecvError::Disconnected) => log_1(&"no2".into()),
+            //}
+            //}
+            //}
 
             //ui.with_layout(egui::Layout::bottom_up(egui::Align::LEFT), |ui| {
             ui.horizontal(|ui| {
@@ -263,7 +262,7 @@ impl eframe::App for TemplateApp {
 
         // match RXG.try_recv() {
         //     Ok(msg) => {
-            
+
         //     //let jsmsg: JsValue = String::from_utf8(msg).unwrap().into();
         //     //log_1(&msg.to_string().into());
         //     log_1(&"something".into());
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
 #![warn(clippy::all, rust_2018_idioms)]
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
-use std::future::Future;
-use rfd::AsyncFileDialog;
-use web_sys::console;
-use wasm_bindgen::JsValue;
-// When compiling natively:
+                                                                   //use rfd::AsyncFileDialog;
+                                                                   //use std::future::Future;
+                                                                   //use wasm_bindgen::JsValue;
+                                                                   //use web_sys::console;
+                                                                   // When compiling natively:
 #[cfg(not(target_arch = "wasm32"))]
 fn main() -> eframe::Result<()> {
     // Log to stdout (if you run with `RUST_LOG=debug`).
@@ -20,6 +20,7 @@ fn main() -> eframe::Result<()> {
 
 // when compiling to web using trunk.
 #[cfg(target_arch = "wasm32")]
+#[allow(clippy::all)]
 fn main() {
     // Make sure panics are logged using `console.error`.
     console_error_panic_hook::set_once();
@@ -39,10 +40,9 @@ fn main() {
     //         // If you care about wasm support you just read() the file
     //         let mystring = String::from_utf8(file.read().await).unwrap();
     //         let js: JsValue = mystring.into();
-    //         console::log_1(&js);      
+    //         console::log_1(&js);
     //     }
     // });
-
 
     let web_options = eframe::WebOptions::default();
 
